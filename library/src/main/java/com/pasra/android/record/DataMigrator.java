25500fac9fa6d222259a2ffa578d9b170b714acb
Richard Plangger <planrichi@gmail.com>
2013-09-22 17:34:12 +0200

    added some docu

--- library/src/main/java/com/pasra/android/record/DataMigrator.java
+++ library/src/main/java/com/pasra/android/record/DataMigrator.java
@@ -10,6 +10,9 @@ public interface DataMigrator {
      * This custom migrator can be used when the normal migrations cannot handle the conversion.
      * Do _NOT_ drop or alter tables. Android record cannot track these changes and this will lead
      * to undefined behaviour.
+     *
+     * It is not advisable to use any generated class here. As this class might disappear in
+     * the app development process.
      * @param db
      * @param currentVersion
      * @param targetVersion
