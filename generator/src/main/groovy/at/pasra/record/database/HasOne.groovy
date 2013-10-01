4479c6601c279ec0ddef4963c78469f6f64f0af1
Richard Plangger <planrichi@gmail.com>
2013-10-01 11:24:17 +0200

    missing class

---
+++ generator/src/main/groovy/at/pasra/record/database/HasOne.groovy
@@ -0,0 +1,60 @@
+package at.pasra.record.database
+
+import at.pasra.record.Inflector
+import at.pasra.record.generation.CodeGenerator
+import org.gradle.api.InvalidUserDataException
+
+/**
+ * Created by rich on 9/13/13.
+ */
+class HasOne extends Relation {
+
+    HasOne(Table origin, Table target) {
+        super(origin, target)
+    }
+
+    @Override
+    void generateSessionMethods(CodeGenerator c) {
+
+    }
+
+    @Override
+    void generateJavaMethods(CodeGenerator c) {
+
+        def javaClassName = target.javaClassName
+        def pluralJavaClassName = Inflector.camelize(Inflector.pluralize(javaClassName))
+        c.wrap("public ${javaClassName} load${javaClassName}(LocalSession session)") {
+            c.line("return session.query${pluralJavaClassName}().where(\"${origin.name}_id = ?\", Long.toString(${origin.primary.javaPrivateFieldName()})).limit(1).first();")
+        }
+    }
+
+    @Override
+    void generateRecordMethods(CodeGenerator c) {
+    }
+
+    @Override
+    void checkIntegrity() {
+        def has_foreign_key = false
+        def type_match = false
+        def type = null
+        def foreign_key = "${origin.name}_id"
+        target.fields.values().each { Field f ->
+            if (f.name == foreign_key) {
+                has_foreign_key = true
+                type = f.type
+                if (f.type == origin.primary.type) {
+                    type_match = true
+                }
+            }
+        }
+
+        if (!has_foreign_key) {
+            throw new InvalidUserDataException("Table ${target.sqlTableName} does not specify a foreign key '${foreign_key}'. " +
+                    "This means that a ${origin.sqlTableName} record does _NOT_ have one ${Inflector.singularize(target.sqlTableName)}!")
+        }
+
+        if (!type_match) {
+            throw new InvalidUserDataException("Table ${target.sqlTableName}: '${foreign_key}' column type is '${type}' but it should be '${origin.primary.type}'. Please change the type!")
+        }
+    }
+}
